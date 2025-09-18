a70d23a8e61b9406faec87cc0ac3ffc2e515ca32
Satvik Jayalwal <satvikjayalwalips@gmail.com>
2025-09-18 10:36:06 +0530

    added palindrome_numbers.py

---
+++ palindrome_numbers.py
@@ -0,0 +1,11 @@
+# Palindrome Numbers
+
+num = int(input("Enter a number : "))
+print(type(num))
+
+if str(num) == str(num)[::-1] :
+    print(f"{num} is a palindrome")
+
+else :
+    print(f"{num} is not a palindrome")
+
