2f0d8188b4f4a105ab5a0de8225726f2bf84e81d
Satvik Jayalwal <satvikjayalwalips@gmail.com>
2025-09-22 11:54:57 +0530

    remove_space_from_string.py

---
+++ remove_space_from_string.py
@@ -0,0 +1,8 @@
+# Remove spaces from a given string
+
+string = input("Enter a string : ")
+
+# Replace spaces with empty string
+no_space = string.replace(" ", "")
+
+print(f"String without spaces: {no_space}")
