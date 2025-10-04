306e5ccc1090b14c81db6f15b5da3d8a5592aa06
Satvik Jayalwal <satvikjayalwalips@gmail.com>
2025-10-04 18:18:24 +0530

    added count_words_containing_a_in_list.py

---
+++ count_words_containing_a_in_list.py
@@ -0,0 +1,11 @@
+# Count Words Containg Letter A
+words = ["apple", "banana", "cherry", "mango", "grape", "kiwi"]
+
+count = 0
+
+for word in words :
+
+    if "a" in word :
+        count += 1
+
+print(count)
