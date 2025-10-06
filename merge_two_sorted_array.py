b39f02213a34842f2134c9eb8607d2d664db19d0
Satvik Jayalwal <satvikjayalwalips@gmail.com>
2025-10-06 14:03:02 +0530

    added merge_two_sorted_array.py

---
+++ merge_two_sorted_array.py
@@ -0,0 +1,12 @@
+# Merge two arrays and sort them
+
+arr1 = [1, 3, 5, 2]
+arr2 = [2, 4, 6]
+
+# Combine both arrays
+merged_array = arr1 + arr2
+
+# Sort the combined array
+merged_array.sort()  # same as Collections.sort()
+
+print(f"Merged and sorted array : {merged_array}")
